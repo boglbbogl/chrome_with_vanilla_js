a49d2ee5dabf7321d8a4037d996974a8070be862
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-08-08 00:18:18 +0900

    input form

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    h1.style.color = "red";
+function onLoginBtnClick() {
+
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here !";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone !";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOffline() {
-    alert("All Gooooood !");
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+loginButton.addEventListener("click", onLoginBtnClick);
