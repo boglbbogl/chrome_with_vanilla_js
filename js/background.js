5c4ceaad74012b710e92c0e89ac5c84d3adeb217
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-08-10 01:44:20 +0900

    code sorting

--- js/background.js
+++ js/background.js
@@ -1,6 +1,4 @@
-const images = [
-    "0.jpg", "1.jpg", "2.jpg"
-];
+const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -8,4 +6,6 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document
+    .body
+    .appendChild(bgImage);
--- js/greetings.js
+++ js/greetings.js
@@ -7,7 +7,9 @@ const USERNAME_KEY = "username";
 
 function onLoginBtnClick(event) {
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm
+        .classList
+        .add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
@@ -15,13 +17,17 @@ function onLoginBtnClick(event) {
 
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting
+        .classList
+        .remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername == null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm
+        .classList
+        .remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginBtnClick);
 } else {
     paintGreetings(savedUsername);
