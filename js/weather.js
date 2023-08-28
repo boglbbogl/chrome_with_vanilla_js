8ce37b5b935cb75db799d799c8e2ecc1b8191827
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2023-08-28 23:56:52 +0900

    end commit

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,13 @@ const API_kEY = "481e3cf815b46e17c5987262746c2fe6";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_kEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_kEY}&units=metric`;
+    fetch(url).then((response) => response.json()).then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
 }
 
 function onGeoError() {
